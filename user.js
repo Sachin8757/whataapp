d59ec333870e056f9fb9b7765c880cc264fb280d
Sachin kumar <130838646+sachinkuamr@users.noreply.github.com>
2024-08-12 20:05:51 +0530

    hello

--- user.js
+++ user.js
@@ -3,8 +3,8 @@ main().then(()=>{
     console.log("connection successfull");
 }).catch((err)=>console.log(err));
 async function main() {
-    // await mongoose.connect("mongodb+srv://sachin:8757887103@shopdata.shpwbu2.mongodb.net/?retryWrites=true&w=majority&appName=whatapp");
-    await mongoose.connect("mongodb://127.0.0.1:27017/whatapp");
+    await mongoose.connect("mongodb+srv://sachin:8757887103@shopdata.shpwbu2.mongodb.net/?retryWrites=true&w=majority&appName=whatapp");
+    // await mongoose.connect("mongodb://127.0.0.1:27017/whatapp");
 }
 
 const user=new mongoose.Schema({
