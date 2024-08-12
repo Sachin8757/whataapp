661f4e81ba26b3c4104b2e5fd4603da11e75c6a2
Sachin kumar <130838646+sachinkuamr@users.noreply.github.com>
2024-08-12 20:04:51 +0530

    new file

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express=require('express');
 const app=express();
 const path=require('path');
-const chat=require('./model');
+const User=require('./user');
 
 app.set("views enging","views");
 app.set("views",path.join(__dirname,"/views"));
@@ -11,46 +11,34 @@ app.use(express.urlencoded({extended:true}))
 
 app.use(methodoverride('_method'));
 
+//login route
+
+app.get("/login",(req,res)=>{
+    res.render("login.ejs")
+})
 
 //index route
 app.get("/",async(req,res)=>{
-    const chats=await chat.find();
-    res.render("index.ejs",{chats});
+    const users=await User.find();
+    res.render("index.ejs",{users});
 })
 //new msg route
+app.get("/msg/:id/user",async(req,res)=>{
+    const {id}=req.params;
+    const user=await User.findById(id);
+    res.render("chat.ejs",{user});
+})
+
 app.get("/new",(req,res)=>{
-res.render("new.ejs");
-})
-app.post("/new",(req,res)=>{
-    const{from,msg,to}=req.body;
-    const chat1=new chat({
-        from:from,
-        msg:msg,
-        to:to
+    const user=new User({
+        name:"sachin",
+        password:"3222"
     })
-    chat1.save();
-    res.redirect("/");
-})
-//delete route
-app.delete("/delete/:id",async(req,res)=>{
-    const {id}=req.params;
-    const deletchat= await chat.findByIdAndDelete(id);
-    res.redirect("/");
-})
-//edit route
-app.get("/chat/:id/edit",async(req,res)=>{
-    const {id}=req.params;
-    const Newchat= await chat.findById(id);
-    res.render("edit.ejs",{Newchat});
-})
-app.patch("/chat/:id/edit",async(req,res)=>{
-    let {id}=req.params;
-  let {Newmsg}=req.body;
-  const Newch=await chat.findByIdAndUpdate(id,{msg:Newmsg});
-    res.redirect("/");
+    user.save();
+    res.redirect("/")
 })
 
 
-app.listen(8080,()=>{
+app.listen(3000,()=>{
     console.log("server runing on port 800...")
 })
---
+++ user.js
@@ -0,0 +1,25 @@
+const mongoose=require('mongoose');
+main().then(()=>{
+    console.log("connection successfull");
+}).catch((err)=>console.log(err));
+async function main() {
+    // await mongoose.connect("mongodb+srv://sachin:8757887103@shopdata.shpwbu2.mongodb.net/?retryWrites=true&w=majority&appName=whatapp");
+    await mongoose.connect("mongodb://127.0.0.1:27017/whatapp");
+}
+
+const user=new mongoose.Schema({
+    name:{
+        type:String
+    },
+    password:{
+        type:String,
+        require:true
+    },
+    // friends:[{
+    //     type:mongoose.Schema.Types.ObjectId,
+    //     // ref:"orders"
+    
+    //   }]
+
+})
+module.exports=mongoose.model("user",user);
